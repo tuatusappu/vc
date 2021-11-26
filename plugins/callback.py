0424a692640b0a2802cd9055ed84b7583df23784
Pratheek <89387738+PratheekXD@users.noreply.github.com>
2021-11-26 06:29:08 +0530

    Create callback.py

---
+++ plugins/callback.py
@@ -0,0 +1,53 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+import config
+from config import BOT_USERNAME
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, Chat, CallbackQuery
+
+
+
+@Client.on_callback_query(filters.regex("cbcmds"))
+async def cbhelps(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""**Group Music Bot : Help Menu**
+
+__× First Add Me To Your Group..
+× Promote Me As Admin In Your Group With All Permission..__
+
+**🏷 Common Commands.**
+
+• `/play` - Song Name : __Plays Via Youtube__
+• `/dplay` - Song Name : __Play Via Deezer__
+• `/splay` - Song Name : __Play Via Jio Saavn__
+• `/playlist` - __Show now playing list__
+• `/current` - __Show now playing__
+
+• `/song` - Song Name : __Get The Song From YouTube__
+• `/vid` - Video Name : __Get The Video From YouTube__
+• `/deezer` - song name : __download songs you want quickly via deezer__
+• `/saavn` - song name : __download songs you want quickly via saavn__
+• `/search` - YouTube Title : __(Get YouTube Search Query)__
+
+**🏷 Group Admin Commands.**
+
+• `/skip` : __Skips Music__
+• `/pause` : __Pause Playing Music__
+• `/resume` : __Resume Playing Music__
+• `/end` : __Stops playing Music__
+• `/reload` : __Reloads Admin List__
+• `/userbotjoin` : __Assistant Joins The Group__
+• `/userbotleave` : __Assistant Leaves From The Group.__""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(text="🎙️ Support Group 🎙️", url="https://t.me/SHIZUKA_VC_SUPPORT"),
+                    InlineKeyboardButton(text="📣 Channel", url=f"https://t.me/aboutpratheek")
+                ],
+                [
+                    InlineKeyboardButton(
+                        "🏡 BACK TO HOME", callback_data="cbstart"
+                    )
+                ]
+            ]
+        )
+    )
