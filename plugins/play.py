d2d93a3d08763eb1570b26b171aa968fc7e6a7a4
Pratheek <89387738+PratheekXD@users.noreply.github.com>
2021-11-27 20:48:21 +0530

    Update play.py

--- plugins/play.py
+++ plugins/play.py
@@ -323,7 +323,7 @@ async def play(_, message: Message):
             return await lel.edit(
                 "🧐 **Song not found! Try searching with the correct title\nExample » /play In The End\n\nChannel : @Pratheek_Bots**"
             )
-        await lel.edit("⚡️")
+        await lel.edit("**Wait Bruh 😎**")
         query = message.text.split(None, 1)[1]
         # print(query)
         await lel.edit("⚡️")
